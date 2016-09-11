3ac345291edbcfaee1ea9f70e2a9209b2a083479
alvaro2aur <alvaro2aur@gmail.com>
2016-09-11 15:42:48 -0400

    slice

--- src/capa/estaciones/CapaEstaciones.java
+++ src/capa/estaciones/CapaEstaciones.java
@@ -24,7 +24,7 @@ public class CapaEstaciones {
     static ArrayList<String> ctrl = new ArrayList<String>();
     static ArrayList<String> est = new ArrayList<String>();
     static ArrayList<String> nombresest = new ArrayList<String>();
-    Double [][] pastel=new Double[40][2];
+    static Double [][] slice=new Double[40][2];
     
     
     public static void main(String[] args) throws IOException {
@@ -32,7 +32,7 @@ public class CapaEstaciones {
         
         String nombreinput="Base de datos_Definitiva 10-05-16.xlsx";     //Direccion+nombre+extencion del archivo de entrada
         String nombreoutput="Estaciones "+fecha.get(Calendar.DATE)+"-"+fecha.get(Calendar.MONTH)+"-"+fecha.get(Calendar.YEAR)+".kml";
-                
+           
         double longarco;//46465/sdsd/sdsdds/sdsd.xlsx
         double aper;
         
@@ -46,7 +46,6 @@ public class CapaEstaciones {
         
         TXT kml = new TXT(nombreoutput);
         
-        XLSX datos2G = new XLSX(nombreinput,0);
 //        XLSX datos3G = new XLSX(nombreinput,1);
 //        XLSX datos4G = new XLSX(nombreinput,2);
 //        XLSX config = new XLSX(nombreinput,3);
@@ -67,7 +66,10 @@ public class CapaEstaciones {
         
         encabezado(kml, nombreoutput);
         estilos(kml, color2G, color3GF1, color3GF2, color4G);
-        hacerKMZ(datos2G, kml);
+        for(int i =0; i<1; i++){
+            XLSX datos = new XLSX(nombreinput,i);
+            hacerKMZ(datos, kml);
+        }
         pie(kml);
         
         
@@ -78,7 +80,7 @@ public class CapaEstaciones {
         String celda, estacion, longitud, latitud, azimut, controladora, cellid, cellid2,
                 TE, TM, altura, antena, cluster;
         ctrl.clear();
-        ctrl = datos.listaColumna(3);
+        ctrl = datos.listaColumna(3);//Crea ArrayList de controladoras
         nombresest.clear();
         nombresest = datos.listaColumna(7);
         
@@ -111,10 +113,9 @@ public class CapaEstaciones {
                 abrirCarpeta(kml, est.get(j));
                 
                 for(int k=1;k<datos.nroDeFilas();k++){
-                    estacion=datos.obtenerCelda(k, 7).substring(0, datos.obtenerCelda(k, 7).length());
+                    estacion=datos.obtenerCelda(k, 7).substring(0, datos.obtenerCelda(k, 7).length()-1);
                     
-                    
-                    if(est.get(j).contains(estacion)){
+                    if(est.get(j).contentEquals(estacion)){
                         celda=datos.obtenerCelda(k, 6);
                         cluster=datos.obtenerCelda(k, 2);
                         cellid=datos.obtenerCelda(k, 4);
@@ -128,8 +129,12 @@ public class CapaEstaciones {
                         altura=datos.obtenerCelda(k, 14);
                         antena=datos.obtenerCelda(k, 20);
                         
+                        slice = crearSlice(azimut, longitud, latitud, cellid);
+                        imprimirSlice(kml, celda, slice, longitud, latitud, azimut, TE, TM, altura, antena, cluster, cellid);
                         
-                    
+                        
+                        
+                        
                         
                     }
                     
@@ -256,6 +261,84 @@ public class CapaEstaciones {
         objeto.escribirLinea("");
     }
 
+    public static void imprimirSlice(TXT objeto, String celda, Double matriz[][], String longitud, String latitud, String azimut, String TE, String TM, String altura, String antena, String cluster, String cellid){
+        /*
+        String estilo="w";
+        switch(N){
+        case 0:
+            estilo="2G";
+            break;
+        case 1:
+            estilo="3G";
+            break;
+        case 2:
+            estilo="4G";
+            break;       
+        }
+        */
+        objeto.escribirLinea("<Placemark>");
+        objeto.escribirLinea("<name>" + celda + " " + cellid + "</name>");
+        //objeto.escribirLinea("<description><![CDATA[LAC = " + lac + "]]></description>");
+        objeto.escribirLinea("<description><![CDATA[Longitud = " + longitud.substring(0, longitud.length()-4) +  "<br> Latitud = " + latitud + "<br> Azimut =  " + azimut + "°" + "<br> TE =  " + TE + "°" + "<br> TM =  " + TM + "°" + "<br> Altura =  " + altura + " m" + "<br> Antena =  " + antena + "<br>" + cluster + "]]></description>");
+        //objeto.escribirLinea("<LookAt>");
+        //objeto.escribirLinea("<longitude>" + longitud +"</longitude>");
+        //objeto.escribirLinea("<latitude>" + latitud + "</latitude>");
+        //objeto.escribirLinea("<altitude>0</altitude>");
+        //objeto.escribirLinea("<range>10</range>");
+        //objeto.escribirLinea("<tilt>0</tilt>");
+        //objeto.escribirLinea("<heading>0</heading>");
+        //objeto.escribirLinea("</LookAt>");
+        objeto.escribirLinea("<styleUrl>#2G</styleUrl>");
+        objeto.escribirLinea("<Polygon>");
+        //objeto.escribirLinea("<altitudeMode>relativeToGround</altitudeMode>");
+        objeto.escribirLinea("<altitudeMode>clampToGround</altitudeMode>");
+        objeto.escribirLinea("<outerBoundaryIs>");
+        objeto.escribirLinea("<LinearRing>");
+        objeto.escribirLinea("<coordinates>");
+        for(byte i=0;i<=20;i++){
+            objeto.escribirLinea(matriz[i][0] + "," + matriz[i][1] + ",0");
+        }
+        objeto.escribirLinea("</coordinates>");
+        objeto.escribirLinea("</LinearRing>");
+        objeto.escribirLinea("</outerBoundaryIs>");
+        objeto.escribirLinea("</Polygon>");
+        objeto.escribirLinea("</Placemark>");
+        objeto.escribirLinea("");
+    }
     
+    public static Double[][] crearSlice(String azimut, String longitud, String latitud, String cellid){
+        Double d, lat, longi, alfa, beta, ang, azi, pasos;
+        
+        d = 0.002;
+        Byte aper = 65;
+        
+        lat = Double.parseDouble(latitud.replace(",", "."));
+        longi = Double.parseDouble(longitud.replace(",", "."));
+        azi = Double.parseDouble(azimut.replace(",", "."));
+                
+        slice[0][0] =longi;
+        slice[0][1] = lat;
+        
+        alfa = (azi+(aper/2))*Math.PI/180;
+        beta = (azi-(aper/2))*Math.PI/180;
+        
+        ang=beta;
+        pasos=(alfa-beta)/19;
+        
+        
+        
+        for (byte i=1; i<=19; i++){
+            //System.out.println(i);
+            slice[i][0]=longi+(d*Math.cos(ang));
+            slice[i][1]=lat+(d*Math.sin(ang));
+            ang=ang+pasos;
+        }
+        
+        slice[20][0]=longi;
+        slice[20][1]=lat;
+        
+        return slice;
+        
+        }
 }
 
